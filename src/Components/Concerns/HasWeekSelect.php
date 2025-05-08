7139d1ca444a093ad69614b04b7f24f9b32229fc
Dreamex <dsotelo@noraitec.com>
2025-05-08 15:22:19 +0200

    Fix: Hasweekselect

--- src/Components/Concerns/HasWeekSelect.php
+++ src/Components/Concerns/HasWeekSelect.php
@@ -17,19 +17,19 @@ trait HasWeekSelect
                 'altInput' => true,
                 'altFormat' => 'W \\| \\d\\e F \\d\\e Y - \\d\\e F \\d\\e Y',
                 'onChange' => 'function(selectedDates, dateStr, instance) {
-                    const weekNumber = dateStr.split("-")[1];
-                    const year = dateStr.split("-")[0];
-                    const startDate = new Date(year, 0, 1 + (weekNumber - 1) * 7);
-                    const endDate = new Date(startDate);
-                    endDate.setDate(startDate.getDate() + 6);
-                    
-                    // Formatear las fechas
-                    const options = { year: "numeric", month: "long", day: "numeric" };
-                    const startFormatted = startDate.toLocaleDateString("es-ES", options);
-                    const endFormatted = endDate.toLocaleDateString("es-ES", options);
-                    
-                    instance._input.value = `Semana ${weekNumber} | ${startFormatted} - ${endFormatted}`;
-                }',
+                const weekNumber = dateStr.split("-")[1];
+                const year = dateStr.split("-")[0];
+                const startDate = new Date(year, 0, 1 + (weekNumber - 1) * 7);
+                const endDate = new Date(startDate);
+                endDate.setDate(startDate.getDate() + 6);
+                
+                // Formatear las fechas
+                const options = { year: "numeric", month: "long", day: "numeric" };
+                const startFormatted = startDate.toLocaleDateString("es-ES", options);
+                const endFormatted = endDate.toLocaleDateString("es-ES", options);
+                
+                instance._input.value = `Semana ${weekNumber} | ${startFormatted} - ${endFormatted}`;
+            }',
             ]);
         }
 
