0b081bdbc417685a155015f97e683a97ce267c85
Dreamex <dsotelo@noraitec.com>
2025-05-08 15:44:44 +0200

    Fix: Hasweekselect

--- src/Components/Concerns/HasWeekSelect.php
+++ src/Components/Concerns/HasWeekSelect.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Noraitec Filament Flatpickr Plugin.
  *
@@ -8,6 +9,7 @@
  * that is bundled with this source code in the LICENSE file.
  * For details see <https://www.gnu.org/licenses/lgpl-3.0.html>
  */
+
 namespace Noraitec\FilamentFlatpickrPlugin\Components\Concerns;
 
 use Carbon\Carbon;
@@ -24,24 +26,22 @@ trait HasWeekSelect
                 'dateFormat' => 'Y-W', // Año y semana
                 'altInput' => true,
                 'altFormat' => 'W \\| \\d\\e F \\d\\e Y - \\d\\e F \\d\\e Y',
-                'onChange' => <<<JS
-                function(selectedDates, dateStr, instance) {
-                    const [year, weekNumber] = dateStr.split("-");
-                    
-                    // Calcular fechas de inicio y fin de semana
-                    const startDate = new Date(year, 0, 1 + (weekNumber - 1) * 7);
-                    const endDate = new Date(startDate);
-                    endDate.setDate(startDate.getDate() + 6);
-                    
-                    // Formatear las fechas
-                    const options = { year: "numeric", month: "long", day: "numeric" };
-                    const startFormatted = startDate.toLocaleDateString("es-ES", options);
-                    const endFormatted = endDate.toLocaleDateString("es-ES", options);
-                    
-                    // Asignar el valor al campo
-                    instance._input.value = 'Semana ' + weekNumber + ' | ' + startFormatted + ' - ' + endFormatted;
-                }
-                JS,
+                'onChange' => "function(selectedDates, dateStr, instance) {
+                const [year, weekNumber] = dateStr.split('-');
+                
+                // Calcular fechas de inicio y fin de semana
+                const startDate = new Date(year, 0, 1 + (weekNumber - 1) * 7);
+                const endDate = new Date(startDate);
+                endDate.setDate(startDate.getDate() + 6);
+                
+                // Formatear las fechas
+                const options = { year: 'numeric', month: 'long', day: 'numeric' };
+                const startFormatted = startDate.toLocaleDateString('es-ES', options);
+                const endFormatted = endDate.toLocaleDateString('es-ES', options);
+                
+                // Asignar el valor al campo
+                instance._input.value = 'Semana ' + weekNumber + ' | ' + startFormatted + ' - ' + endFormatted;
+            }",
             ]);
         }
 
